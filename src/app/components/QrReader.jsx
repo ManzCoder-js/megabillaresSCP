4a59984a4f0e42a25ff9e05e2910e84f46d7b7ab
hectormanza <mahector420@gmail.com>
2023-06-03 18:11:32 -0600

    modificados:     src/app/components/QrReader.jsx

--- src/app/components/QrReader.jsx
+++ src/app/components/QrReader.jsx
@@ -14,7 +14,6 @@ const QrReader = () => {
           throw new Error('getUserMedia is not supported');
         }
         const constraints = {
-          facingMode: 'environment',
           video: true,
           audio: false
         };
