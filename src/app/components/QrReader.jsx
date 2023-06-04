c5eb602d3743a207fff535c18b69ea25894bea66
hectormanza <mahector420@gmail.com>
2023-06-03 18:31:35 -0600

    modificados:     src/app/components/QrReader.jsx

--- src/app/components/QrReader.jsx
+++ src/app/components/QrReader.jsx
@@ -6,6 +6,7 @@ const QrReader = () => {
   const canvasRef = useRef(null);
   const [errorMessage, setErrorMessage] = useState('');
   const [qrCodeData, setQrCodeData] = useState('');
+  const [isCameraStarted, setIsCameraStarted] = useState(false);
 
   useEffect(() => {
     const startCamera = async () => {
@@ -14,14 +15,13 @@ const QrReader = () => {
           throw new Error('getUserMedia is not supported');
         }
         const constraints = {
-            video: true,
-            audio: false
-          };
+          video: true,
+          audio: false
+        };
 
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
 
         videoRef.current.srcObject = stream;
-        videoRef.current.play();
       } catch (error) {
         console.error('Error accessing camera:', error);
         setErrorMessage('Failed to access camera');
@@ -39,31 +39,54 @@ const QrReader = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const videoElement = videoRef.current;
+
+    const handleVideoLoadedMetadata = () => {
+      setIsCameraStarted(true);
+      videoElement.play();
+    };
+
+    videoElement.addEventListener('loadedmetadata', handleVideoLoadedMetadata);
+
+    return () => {
+      videoElement.removeEventListener('loadedmetadata', handleVideoLoadedMetadata);
+    };
+  }, []);
+
   const scanQRCode = () => {
     const video = videoRef.current;
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d', { willReadFrequently: true });
 
-    // Establecer la resolución del lienzo para que coincida con la resolución de la cámara
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    if (video && video.videoWidth && video.videoHeight) {
+      // Establecer la resolución del lienzo para que coincida con la resolución de la cámara
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
 
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    const code = jsQR(imageData.data, imageData.width, imageData.height);
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height);
 
-    if (code) {
-      setQrCodeData(code.data);
-    } else {
-      console.log('No QR Code found.');
+      if (code) {
+        setQrCodeData(code.data);
+      } else {
+        console.log('No QR Code found.');
+      }
     }
   };
 
   return (
     <div>
       {errorMessage && <p>{errorMessage}</p>}
-      <video ref={videoRef} autoPlay><button onClick={scanQRCode}>Scan QR Code</button></video>
-      <canvas ref={canvasRef} style={{ display: 'none' }} />
+      {isCameraStarted ? (
+        <>
+          <canvas ref={canvasRef} style={{ display: 'none' }} />
+          <button onClick={scanQRCode}>Scan QR Code</button>
+        </>
+      ) : (
+        <video ref={videoRef} autoPlay muted></video>
+      )}
       {qrCodeData && <h1>{qrCodeData}</h1>}
     </div>
   );
