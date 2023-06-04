753ed8b92947467b49b75a88197b51b670bb9a5b
hectormanza <mahector420@gmail.com>
2023-06-03 22:41:47 -0600

    modificados:     src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <AuthProvider>
-    <html lang="en">
+    <html lang="es">
       <body >
         <Nav/>
         {children}
