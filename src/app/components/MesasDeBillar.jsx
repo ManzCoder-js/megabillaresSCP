1d5796c73fa77f8b9f0158591ba8b1de50b18300
hectormanza <mahector420@gmail.com>
2023-06-08 06:06:23 -0600

    modificados:     src/app/components/MesasDeBillar.jsx 	modificados:     src/app/components/Ventas.jsx

--- src/app/components/MesasDeBillar.jsx
+++ src/app/components/MesasDeBillar.jsx
@@ -1,69 +1,117 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const MesasDeBillar = ({ onSeleccion }) => {
-  const [modelo, setModelo] = useState('');
-  const [tamaño, setTamaño] = useState('');
-  const [color, setColor] = useState('');
-  const [acabado, setAcabado] = useState('');
+const MesasDeBillar = ({ ventaEditar, onSeleccion, onCancel }) => {
+  const [nuevaVenta, setNuevaVenta] = useState({
+    modelo: '',
+    tamaño: '',
+    color: '',
+    acabado: '',
+    cliente: '',
+  });
 
-  const handleSeleccion = () => {
-    const nuevaVenta = {
-      modelo,
-      tamaño,
-      color,
-      acabado,
-    };
+  useEffect(() => {
+    if (ventaEditar) {
+      setNuevaVenta(ventaEditar);
+    }
+  }, [ventaEditar]);
+
+  const handleChange = (field, value) => {
+    setNuevaVenta({
+      ...nuevaVenta,
+      [field]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     onSeleccion(nuevaVenta);
-    setModelo('');
-    setTamaño('');
-    setColor('');
-    setAcabado('');
+    setNuevaVenta({
+      modelo: '',
+      tamaño: '',
+      color: '',
+      acabado: '',
+      cliente: '',
+    });
+  };
+
+  const handleCancel = () => {
+    setNuevaVenta({
+      modelo: '',
+      tamaño: '',
+      color: '',
+      acabado: '',
+      cliente: '',
+    });
+    onCancel(); // Llamada a la función onCancel para cerrar el formulario
   };
 
   return (
     <div>
-      <h3>Seleccionar opciones de personalización</h3>
-      <label>
-        Modelo:
-        <select value={modelo} onChange={(e) => setModelo(e.target.value)}>
-          <option value="">Seleccione un modelo</option>
-          <option value="Modelo 1">Modelo 1</option>
-          <option value="Modelo 2">Modelo 2</option>
-          <option value="Modelo 3">Modelo 3</option>
-          {/* Agrega más opciones de modelo aquí */}
-        </select>
-      </label>
-      <label>
-        Tamaño:
-        <select value={tamaño} onChange={(e) => setTamaño(e.target.value)}>
-          <option value="">Seleccione un tamaño</option>
-          <option value="Tamaño 1">Tamaño 1</option>
-          <option value="Tamaño 2">Tamaño 2</option>
-          <option value="Tamaño 3">Tamaño 3</option>
-          {/* Agrega más opciones de tamaño aquí */}
-        </select>
-      </label>
-      <label>
-        Color:
-        <select value={color} onChange={(e) => setColor(e.target.value)}>
-          <option value="">Seleccione un color</option>
-          <option value="Color 1">Color 1</option>
-          <option value="Color 2">Color 2</option>
-          <option value="Color 3">Color 3</option>
-          {/* Agrega más opciones de color aquí */}
-        </select>
-      </label>
-      <label>
-        Acabado:
-        <select value={acabado} onChange={(e) => setAcabado(e.target.value)}>
-          <option value="">Seleccione un acabado</option>
-          <option value="Acabado 1">Acabado 1</option>
-          <option value="Acabado 2">Acabado 2</option>
-          <option value="Acabado 3">Acabado 3</option>
-          {/* Agrega más opciones de acabado aquí */}
-        </select>
-      </label>
-      <button onClick={handleSeleccion}>Seleccionar</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Modelo:
+          <select
+            value={nuevaVenta.modelo}
+            onChange={(e) => handleChange('modelo', e.target.value)}
+          >
+            <option value="">Seleccione un modelo</option>
+            <option value="Modelo 1">Modelo 1</option>
+            <option value="Modelo 2">Modelo 2</option>
+            <option value="Modelo 3">Modelo 3</option>
+            {/* Agrega más opciones de modelo aquí */}
+          </select>
+        </label>
+        <label>
+          Tamaño:
+          <select
+            value={nuevaVenta.tamaño}
+            onChange={(e) => handleChange('tamaño', e.target.value)}
+          >
+            <option value="">Seleccione un tamaño</option>
+            <option value="Tamaño 1">Tamaño 1</option>
+            <option value="Tamaño 2">Tamaño 2</option>
+            <option value="Tamaño 3">Tamaño 3</option>
+            {/* Agrega más opciones de tamaño aquí */}
+          </select>
+        </label>
+        <label>
+          Color:
+          <select
+            value={nuevaVenta.color}
+            onChange={(e) => handleChange('color', e.target.value)}
+          >
+            <option value="">Seleccione un color</option>
+            <option value="Color 1">Color 1</option>
+            <option value="Color 2">Color 2</option>
+            <option value="Color 3">Color 3</option>
+            {/* Agrega más opciones de color aquí */}
+          </select>
+        </label>
+        <label>
+          Acabado:
+          <select
+            value={nuevaVenta.acabado}
+            onChange={(e) => handleChange('acabado', e.target.value)}
+          >
+            <option value="">Seleccione un acabado</option>
+            <option value="Acabado 1">Acabado 1</option>
+            <option value="Acabado 2">Acabado 2</option>
+            <option value="Acabado 3">Acabado 3</option>
+            {/* Agrega más opciones de acabado aquí */}
+          </select>
+        </label>
+        <label>
+          Cliente:
+          <input
+            type="text"
+            name="cliente"
+            value={nuevaVenta.cliente}
+            onChange={(e) => handleChange('cliente', e.target.value)}
+          />
+        </label>
+        <button type="submit">{ventaEditar ? 'Guardar Cambios' : 'Crear Venta'}</button>
+        <button type="button" onClick={handleCancel}>Cancelar</button>
+      </form>
     </div>
   );
 };
--- src/app/components/Ventas.jsx
+++ src/app/components/Ventas.jsx
@@ -4,14 +4,41 @@ import MesasDeBillar from './MesasDeBillar';
 const Ventas = () => {
   const [ventas, setVentas] = useState([]);
   const [mostrarMesasDeBillar, setMostrarMesasDeBillar] = useState(false);
+  const [ventaEditar, setVentaEditar] = useState(null); // Estado para almacenar la venta actualmente seleccionada para su edición
 
   const handleCrearVenta = (nuevaVenta) => {
-    setVentas([...ventas, nuevaVenta]);
-    setMostrarMesasDeBillar(false); // Ocultar la tarjeta de MesasDeBillar después de crear la venta
+    if (ventaEditar) {
+      // Si hay una venta seleccionada para editar, actualizamos la venta existente
+      const ventasActualizadas = ventas.map((venta) =>
+        venta === ventaEditar ? nuevaVenta : venta
+      );
+      setVentas(ventasActualizadas);
+      setVentaEditar(null); // Limpiamos el estado de la venta a editar
+    } else {
+      // Si no hay venta seleccionada para editar, agregamos una nueva venta
+      setVentas([...ventas, nuevaVenta]);
+    }
+    setMostrarMesasDeBillar(false); // Ocultar el formulario de MesasDeBillar después de crear/editar la venta
+  };
+
+  const handleEditarVenta = (venta) => {
+    setVentaEditar(venta); // Establecer la venta seleccionada para editar
+    setMostrarMesasDeBillar(true); // Mostrar el formulario de MesasDeBillar para editar la venta
+  };
+
+  const handleEliminarVenta = (venta) => {
+    const ventasActualizadas = ventas.filter((v) => v !== venta);
+    setVentas(ventasActualizadas);
   };
 
   const handleMostrarMesasDeBillar = () => {
+    setVentaEditar(null); // Limpiamos el estado de la venta a editar
     setMostrarMesasDeBillar(true);
+  };
+
+  const handleCancelarFormulario = () => {
+    setVentaEditar(null); // Limpiamos el estado de la venta a editar
+    setMostrarMesasDeBillar(false);
   };
 
   return (
@@ -22,8 +49,12 @@ const Ventas = () => {
       )}
       {mostrarMesasDeBillar && (
         <div className="venta-card">
-          <h3>Nueva Venta</h3>
-          <MesasDeBillar onSeleccion={handleCrearVenta} />
+          <h3>{ventaEditar ? 'Editar Venta' : 'Nueva Venta'}</h3>
+          <MesasDeBillar
+            ventaEditar={ventaEditar} // Pasamos la venta a editar como prop
+            onSeleccion={handleCrearVenta}
+            onCancel={handleCancelarFormulario}
+          />
         </div>
       )}
       {ventas.map((venta, index) => (
@@ -33,6 +64,8 @@ const Ventas = () => {
           <p>Color: {venta.color}</p>
           <p>Acabado: {venta.acabado}</p>
           <p>Cliente: {venta.cliente}</p>
+          <button onClick={() => handleEditarVenta(venta)}>Editar</button> {/* Agregamos el botón de Editar */}
+          <button onClick={() => handleEliminarVenta(venta)}>Eliminar</button> {/* Agregamos el botón de Eliminar */}
         </div>
       ))}
     </div>
